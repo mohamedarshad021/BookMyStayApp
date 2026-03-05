f9339678ccd599ec6c2a422ddd1e9f5c8bd2306f
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-05 14:38:30 +0530

    RA2411026011187 Added UC2-AddRoom

--- RA2411026011187/App/src/BookMyStayApp.java
+++ RA2411026011187/App/src/BookMyStayApp.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public class BookMyStayApp {
+
+    static ArrayList<String> rooms = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -7,5 +11,18 @@ public class BookMyStayApp {
         System.out.println("=================================");
         System.out.println("Hotel Booking Management System");
 
+        // UC2 Logic
+        addRoom("101");
+        addRoom("102");
+
+        System.out.println("\nAvailable Rooms:");
+
+        for(String room : rooms){
+            System.out.println("Room No: " + room);
+        }
+    }
+
+    public static void addRoom(String roomNo){
+        rooms.add(roomNo);
     }
 }
