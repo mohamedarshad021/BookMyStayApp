6378bd0a65e1b4bef02dd7224ef9f36a5e2c30f9
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-05 14:26:43 +0530

    RA2411026011187 Added UC1-WelcomePage

--- RA2411026011187/App/src/BookMyStayApp.java
+++ RA2411026011187/App/src/BookMyStayApp.java
@@ -3,9 +3,9 @@ public class BookMyStayApp {
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println("     Welcome to Book My Stay     ");
+        System.out.println("       Welcome to BookMyStay     ");
         System.out.println("=================================");
-        System.out.println("Your smart hotel booking system");
+        System.out.println("Hotel Booking Management System");
 
     }
 }
